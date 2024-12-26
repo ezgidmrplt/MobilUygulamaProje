17633bda2f3b53dd140ca2f3bcfe56670fd7d469
Ezgi Nur Demirpolat <132230082@ogr.uludag.edu.tr>
2024-12-26 18:56:48 +0300

    deneme

---
+++ mobil_proje_1/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'anasayfa.dart'; // Giriş ekranı dosyasını çağırıyoruz
+
+void main() {
+  runApp(ToplantiUygulamasi());
+}
+
+class ToplantiUygulamasi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GirisEkrani(),
+    );
+  }
+}
