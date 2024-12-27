003ac82bd6d6cf19a2f731c913dd16f78c1de34d
Ezgi Nur Demirpolat <132230082@ogr.uludag.edu.tr>
2024-12-27 19:25:43 +0300

    firebase kuruldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mobil_proje_1/firebase_options.dart';
 import 'anasayfa.dart'; // Giriş ekranı dosyasını çağırıyoruz
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ToplantiUygulamasi());
 }
 
